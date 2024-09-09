891240c53bdc4ed49d9add340c8d727b426b72a8
spiritualized <contact@spiritualized.io>
2024-09-08 21:05:24 -0400

    Upgrade libav to v13.0.0

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='libprick',
     packages=['libprick'],
-    version='1.2.1',
+    version='1.3.0',
     description='Python interface to the Last.FM API/website with caching support',
     url='https://github.com/spiritualized/libprick',
     download_url='https://github.com/libprick/lastfmcache/archive/v1.2.0.tar.gz',
     keywords=['pricker', 'python', 'hash', 'sha256'],
     install_requires=[
-                    'av==10.0.0'
+                    'av==13.0.0'
                 ],
 
     classifiers=[
