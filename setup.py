9422dd66e75a08c3ee756363970c9268d755de71
spiritualized <contact@spiritualized.io>
2026-01-28 13:53:15 -0500

    Upgrade libav to v16.1.0

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from distutils.core import setup
 setup(
     name='libprick',
     packages=['libprick'],
-    version='1.3.0',
+    version='1.4.0',
     description='Python interface to the Last.FM API/website with caching support',
     url='https://github.com/spiritualized/libprick',
-    download_url='https://github.com/libprick/lastfmcache/archive/v1.2.0.tar.gz',
+    download_url='https://github.com/libprick/lastfmcache/archive/v1.4.0.tar.gz',
     keywords=['pricker', 'python', 'hash', 'sha256'],
     install_requires=[
-                    'av==13.0.0'
+                    'av==16.1.0'
                 ],
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.14',
     ],
 )
